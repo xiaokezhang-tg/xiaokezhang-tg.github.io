da9bc8f9abf228dea80d7cf5a92c772fc2ab2be8
xiaokezhang-tg <xiaokezhang-tg@users.noreply.github.com>
2026-06-14 06:06:45 +0000

    Deploying to gh-pages from @ xiaokezhang-tg/xiaokezhang-tg.github.io@8dfce98e9245b939bc93299c661356274e758334 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://xiaokezhang-tg.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://xiaokezhang.com/#contact">contact us</a>.'
               }
             ]
           }
